2be74cd79031ad584e42a0b67c79c6811217ce30
Shakhmurad <muradb356@gmail.com>
2023-08-28 15:33:37 +0400

    add element in homePage

--- src/NavbarMetodlar/HomePage/HomeCard.jsx
+++ src/NavbarMetodlar/HomePage/HomeCard.jsx
@@ -1,65 +1,17 @@
 import React from 'react'
-import img1 from "../../assets/img/Icon.svg"
-import img2 from "../../assets/img/Icon (1).svg"
-import img3 from "../../assets/img/Icon (2).svg"
-import img4 from "../../assets/img/Icon (3).svg"
-import img5 from "../../assets/img/Icon (4).svg"
-import img6 from "../../assets/img/Icon (5).svg"
+import data from './data'
 
 function HomeCard() {
-
-    const Card =[
-
-        {
-            id:1,
-            item:'Uses Client First',
-            imge:img1,
-            text:'Euismod faucibus turpis eu gravida mi. Pellentesque et velit aliquam sed faucib turpis eu gravida mi. Pellentesque et velit aliquam sed mi. '
-        },
-        {
-            id:2,
-            item:'Two Free Revision Round',
-            imge:img2,
-            text:'Euismod faucibus turpis eu gravida mi. Pellentesque et velit aliquam sed faucib turpis eu gravida mi. Pellentesque et velit aliquam sed mi. . '
-        },
-        {
-            id:3,
-            item:'Template Customization',
-            imge:img3,
-            text:'Euismod faucibus turpis eu gravida mi. Pellentesque et velit aliquam sed faucib turpis eu gravida mi. Pellentesque et velit aliquam sed mi. '
-        },
-        {
-            id:4,
-            item:'24/7 Support',
-            imge:img4,
-            text:'Euismod faucibus turpis eu gravida mi. Pellentesque et velit aliquam sed faucib turpis eu gravida mi. Pellentesque et velit aliquam sed mi. '
-        },
-        {
-            id:5,
-            item:'Quick Delivery',
-            imge:img5,
-            text:'Euismod faucibus turpis eu gravida mi. Pellentesque et velit aliquam sed faucib turpis eu gravida mi. Pellentesque et velit aliquam sed mi. '
-        },
-        {
-            id:6,
-            item:'Hands-on approach',
-            imge:img6,
-            text:'Euismod faucibus turpis eu gravida mi. Pellentesque et velit aliquam sed faucib turpis eu gravida mi. Pellentesque et velit aliquam sed mi. '
-        },
-    ]
-
-
-
   return (
     <>
    
    <div className="Card-container">
         {
-            Card.map(({id , imge , text, item})=>{
+            data.map(({id , imge , text, item})=>{
                 return(
 
                         <div className="card">
-                            <img src={imge} />
+                            <img src={`../../src/assets/img/${imge}`} />
                             <h2>{item}</h2>
                             <p>{text}</p>
                         </div>
---
+++ src/NavbarMetodlar/HomePage/data.js
@@ -0,0 +1,40 @@
+
+export default [
+
+    {
+        id:1,
+        item:'Uses Client First',
+        imge:"Icon.svg",
+        text:'Euismod faucibus turpis eu gravida mi. Pellentesque et velit aliquam sed faucib turpis eu gravida mi. Pellentesque et velit aliquam sed mi. '
+    },
+    {
+        id:2,
+        item:'Two Free Revision Round',
+        imge:'Icon (1).svg',
+        text:'Euismod faucibus turpis eu gravida mi. Pellentesque et velit aliquam sed faucib turpis eu gravida mi. Pellentesque et velit aliquam sed mi. . '
+    },
+    {
+        id:3,
+        item:'Template Customization',
+        imge:'Icon (2).svg',
+        text:'Euismod faucibus turpis eu gravida mi. Pellentesque et velit aliquam sed faucib turpis eu gravida mi. Pellentesque et velit aliquam sed mi. '
+    },
+    {
+        id:4,
+        item:'24/7 Support',
+        imge:'Icon (3).svg',
+        text:'Euismod faucibus turpis eu gravida mi. Pellentesque et velit aliquam sed faucib turpis eu gravida mi. Pellentesque et velit aliquam sed mi. '
+    },
+    {
+        id:5,
+        item:'Quick Delivery',
+        imge:'Icon (4).svg',
+        text:'Euismod faucibus turpis eu gravida mi. Pellentesque et velit aliquam sed faucib turpis eu gravida mi. Pellentesque et velit aliquam sed mi. '
+    },
+    {
+        id:6,
+        item:'Hands-on approach',
+        imge:'Icon (5).svg',
+        text:'Euismod faucibus turpis eu gravida mi. Pellentesque et velit aliquam sed faucib turpis eu gravida mi. Pellentesque et velit aliquam sed mi. '
+    },
+]
