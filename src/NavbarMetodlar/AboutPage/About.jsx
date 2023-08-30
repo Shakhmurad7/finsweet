14d9ac3f0671945fdf74d8dd34e9479a003f7e8c
Shakhmurad <muradb356@gmail.com>
2023-08-30 17:02:37 +0400

    add seclect-center in about

--- src/NavbarMetodlar/AboutPage/About.jsx
+++ src/NavbarMetodlar/AboutPage/About.jsx
@@ -85,6 +85,7 @@ const About =()=>{
                     <div className={styles['section-center']}>
                         <div className="Big-container">
                             <div className={styles['section-center-container']}>
+
                                 <div className={styles["section-center-row"]}>
                                     <div className={styles['section-center-left']}>
                                         <h3>Our Mission </h3>
@@ -95,6 +96,18 @@ const About =()=>{
                                         <img src={'public/img/a-man-standing-in-an-office-7792811.png'}/>
                                     </div>
                                 </div>
+
+                                <div className={styles["section-center-row"]}>
+                                    <div className={styles['section-center-right']}>
+                                        <img src={'public/img/photo-of-women-at-the-meeting-3810789.png'}/>
+                                    </div>
+                                    <div className={styles['section-center-left']}>
+                                        <h3>Our Mission </h3>
+                                        <h2>Laser focus</h2>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                                    </div>
+                                </div>
+
                             </div>
                         </div>
                     </div>
